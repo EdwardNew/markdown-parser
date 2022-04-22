9c2f069b0e0c48edc4bb144c1ff6df07711996c7
EdwardNew <pkarrows@gmail.com>
2022-04-21 17:07:38 -0700

    Added a Junit test for test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,19 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+import java.nio.file.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void mdTest1() throws Exception{
+        List expected = List.of("https://something.com", "some-thing.html");
+
+        Path filepath = Path.of("test-file.md");
+        String file = Files.readString(filepath);
+        assertEquals(expected, MarkdownParse.getLinks(file));
     }
 }
