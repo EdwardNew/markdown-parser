0f83505e176b3d62891fb82f8d648fc51f0572eb
EdwardNew <pkarrows@gmail.com>
2022-05-22 20:33:08 -0700

    added test snippet files and tests for lab report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -107,4 +107,34 @@ public class MarkdownParseTest {
         assertEquals(expected, MarkdownParse.getLinks(file));
     }
 
+    //Tests for Lab Report 4
+    @Test
+    public void labTestSnippet1() throws Exception{
+        List expected = List.of("`google.com", "google.com", "ucsd.edu");
+
+        Path filepath = Path.of("lab-report-snippet1.md");
+        String file = Files.readString(filepath);
+        assertEquals(expected, MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void labTestSnippet2() throws Exception{
+        List expected = List.of("a.com", "a.com(())", "exmaple.com");
+
+        Path filepath = Path.of("lab-report-snippet2.md");
+        String file = Files.readString(filepath);
+        assertEquals(expected, MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void labTestSnippet3() throws Exception{
+        List expected = List.of("https://www.twitter.com",
+        "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule",
+        "https://cse.ucsd.edu/");
+
+        Path filepath = Path.of("lab-report-snippet3.md");
+        String file = Files.readString(filepath);
+        assertEquals(expected, MarkdownParse.getLinks(file));
+    }
+
 }
